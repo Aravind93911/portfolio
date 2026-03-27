5c59cda8b1a766888aeca5c13f81d963b79a4272
Dhakuri Aravind <aravinddhakuri@gmail.com>
2026-03-27 18:50:01 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// simple fade-in animation
-document.addEventListener("scroll",()=>{
-document.querySelectorAll(".card").forEach(c=>{
-let rect=c.getBoundingClientRect();
-if(rect.top<window.innerHeight){
-c.style.opacity=1;
-}
-});
-});
